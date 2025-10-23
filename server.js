8508ac2074d5f7f8e55f71999224c6e93939076d
rosario1424 <rosariocse24@gmail.com>
2025-10-23 13:43:26 +0530

    database connectivity --done

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 // import express module
 const express=require('express');
+const mongoose=require('mongoose');
+require('dotenv').config();
 
 // create an express application
 const app=express();
@@ -12,6 +14,12 @@ app.get('/', (req, res) =>{
 app.listen(3001,'localhost',() => {
     console.log(`server is running on http://localhost:3001`);
 });
+
+// connect to MongoDB
+mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => console.log('connected to MongoDB'))
+    .catch((err) => console.error('could not connect to MongoDB', err));
 
 
 
