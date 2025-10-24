2748c10a6bcb53b5828b4b8dfa0ee853625824c9
rosario1424 <rosariocse24@gmail.com>
2025-10-24 20:17:14 +0530

    update recipes and deleted recipes --done

--- controllers/recipesControllers.js
+++ controllers/recipesControllers.js
@@ -35,6 +35,43 @@ try {
   }
 }
 
+const updateRecipes = async (req, res) => {
+      try {
+        // get the id from the url params
+        const id = req.params.id;
 
+        // get the data to update from the request body
+        const updateData = req.body;
 
-module.exports={getAllRecipes, createRecipesApp, getRecipesById}
+        // find the recipe by id and update it with the new data
+        const updatedRecipes = await Recipes.findByIdAndUpdate(id, updateData, { new: true });
+
+        if (!updatedRecipes) {
+          return res.status(404).json({ message: 'Recipes not found' });
+        }
+
+        res.status(200).json ({ message: 'Recipes Updated', recipes: updateRecipes });
+      } catch (error) {
+          res.status(500).json({ message: 'Updating Recipes failed...', error: error.message });
+      }
+}
+
+const deleteRecipes = async (req, res) => {
+     try {
+          // get the id from the url params
+          const id = req.params.id;
+
+          const recipesDeleted = await Recipes.findByIdAndDelete(id);
+
+          if (!recipesDeleted) {
+               return res.status(404).json ({ messgae: 'Recipes not found'});
+          }
+
+          res.status(200).json({ message: 'Recipes Deleted', recipes: recipesDeleted});
+
+     } catch (error) {
+          res.status(500).json({ message: 'Deleting recipes failed...', error: error.message });
+     }
+}
+
+module.exports={getAllRecipes, createRecipesApp, getRecipesById, updateRecipes, deleteRecipes }
--- routes/recipesRoutes.js
+++ routes/recipesRoutes.js
@@ -1,9 +1,11 @@
 const express=require('express');
-const { getAllRecipes, createRecipesApp, getRecipesById } = require('../controllers/recipesControllers');
+const { getAllRecipes, createRecipesApp, getRecipesById, updateRecipes, deleteRecipes } = require('../controllers/recipesControllers');
 const recipesRouter=express.Router();
 
 recipesRouter.post('/',createRecipesApp);
 recipesRouter.get('/',getAllRecipes);
 recipesRouter.get('/:id', getRecipesById);
+recipesRouter.put('/:id', updateRecipes);
+recipesRouter.delete('/:id', deleteRecipes);    
 
 module.exports=recipesRouter;
